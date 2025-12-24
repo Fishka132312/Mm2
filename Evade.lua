cf06249aa642562a478f2fe53c5d7a2de164ae20
Fishka132312 <144502163+Fishka132312@users.noreply.github.com>
2025-12-24 13:44:53 +0100

    Update Evade.lua

--- Evade.lua
+++ Evade.lua
@@ -209,36 +209,55 @@ Tab:AddButton({
 	Name = "Fps boost",
 	Callback = function()
     local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
 local lighting = game:GetService("Lighting")
 local terrain = workspace:FindFirstChildOfClass("Terrain")
 
+local active = true
+
+UserInputService.InputBegan:Connect(function(input, gameProcessed)
+    if input.KeyCode == Enum.KeyCode.LeftControl or input.KeyCode == Enum.KeyCode.RightControl then
+        active = false
+        print("FPS Boost остановлен.")
+    end
+end)
+
+settings().Rendering.QualityLevel = 1
 lighting.GlobalShadows = false
 lighting.FogEnd = 9e9
-settings().Rendering.QualityLevel = 1
-
-for _, v in pairs(lighting:GetChildren()) do
-    if v:IsA("PostEffect") or v:IsA("BloomEffect") or v:IsA("BlurEffect") or v:IsA("SunRaysEffect") then
-        v.Enabled = false
-    end
-end
-
-for _, v in pairs(game:GetDescendants()) do
-    if v:IsA("Part") or v:IsA("MeshPart") or v:IsA("UnionOperation") then
-        v.Material = Enum.Material.SmoothPlastic
-        v.Reflectance = 0
-    elseif v:IsA("Decal") or v:IsA("Texture") then
-        v:Destroy()
-    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-        v.Enabled = false
-    end
-end
+lighting.Brightness = 1 
 
 if terrain then
     terrain.WaterWaveSize = 0
     terrain.WaterWaveSpeed = 0
     terrain.WaterReflectance = 0
     terrain.WaterTransparency = 0
+    terrain.Decoration = false 
 end
+
+local function cleanUp(v)
+    if not active then return end
+    
+    if v:IsA("Part") or v:IsA("MeshPart") or v:IsA("UnionOperation") then
+        v.Material = Enum.Material.SmoothPlastic
+        v.Reflectance = 0
+    elseif v:IsA("Decal") or v:IsA("Texture") then
+        v:Destroy()
+    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") or v:IsA("Explosion") then
+        v.Enabled = false
+    elseif v:IsA("PostEffect") or v:IsA("BloomEffect") or v:IsA("BlurEffect") or v:IsA("SunRaysEffect") then
+        v.Enabled = false
+    end
+end
+
+for _, v in pairs(game:GetDescendants()) do
+    cleanUp(v)
+end
+game.DescendantAdded:Connect(function(v)
+    if active then
+        cleanUp(v)
+    end
+end)
   	end    
 })
 
@@ -250,6 +269,7 @@ Tab:AddButton({
   	end    
 })
 OrionLib:Init()
+
 
 
 
